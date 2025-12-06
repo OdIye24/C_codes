254fa1a285320fb58cc843b6565da1fd82eea61b
OdIye24 <esma.canbasi06@gmail.com>
2025-12-06 20:54:05 +0300

    0dan büyük bir sayının poozitif bölenlerini bulma(Finding positive divisors of a positive number).c

--- 0dan büyük bir sayının poozitif bölenlerini bulma.c
+++ 0dan büyük bir sayının poozitif bölenlerini bulma.c
@@ -4,15 +4,17 @@
 int main()
 {
    int x;
-   printf("0dan buyuk sayi gir\n");
+   while(1){
+   printf("\nenter an integer which is greater than 0(enter -1 to exit)\n");
+   printf("\n0dan buyuk bir sayi girin(programdan çıkmak için -1e basin)\n");
    scanf("%d",&x);
-
    for(int a=1;a<=x;a++){
     if(x%a==0){
-
         printf("%d ",a);
-
-    }
+      }
+   }if(x==-1){
+   break;
    }
-    return 0;
+   }
+   return 0;
 }
